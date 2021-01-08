2e23634a020682e4cbb51c3b3e29f1b668c3d5aa
benedikt <benedikt.oesing@tu-dortmund.de>
2021-01-08 03:45:32 +0100

    Fixed some issues with the code

--- extension/fuzzySearch.js
+++ extension/fuzzySearch.js
@@ -1,29 +1,19 @@
 // Put all the javascript code here, that you want to execute after page load.
-document.addEventListener("keydown", addExtension);
-document.addEventListener("mousedown", addExtension);
+addExtension();
 
 function addExtension() {
-    document.removeEventListener("keydown", addExtension);
-    let searchBars = document.getElementsByClassName("multiselect__input");
-    for(var i = 0; i < searchBars.length; i++)
-    {
-        var element = searchBars[i];
-        if (element.placeholder === "Search Items...")
-        {
-            element.addEventListener("keydown", appendFuzzyToValue);
-            console.log("Search Items... found");
-            
-        }
-        if (element.placeholder === "+ Add Stat Filter")
-        {
-            element.addEventListener("keydown", appendFuzzyToValue);
-            console.log("+ Add Stat Filter found")
-        }
-    }
+        console.log("Added Eventlistener");
+        document.removeEventListener("keydown", addExtension);
+        var searchbar = document.querySelector("#trade > div.top > div.search-panel > div:nth-child(1) > div.search-left > div > div.multiselect__tags > input");
+        var attributeFilter = document.querySelector("#trade > div.top > div.search-panel > div.search-bar.search-advanced > div > div.search-advanced-pane.brown > div.filter-group.expanded > div.filter-group-body > div > span > div > div.multiselect__tags > input");
+        searchbar.addEventListener("keydown", appendFuzzyToValue);
+        attributeFilter.addEventListener("keydown", appendFuzzyToValue);
+        addedEventlistener = true;
 }
 
 function appendFuzzyToValue(element)
 {
+    console.log("Inserted ~ into searchfield");
     if (!element.currentTarget.value.startsWith("~"))
     {
         element.currentTarget.value = "~" + element.currentTarget.value;
