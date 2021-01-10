59c0e14505c0354ed4fcb7af9aad69e6b2fb5e8f
Alexander <senger.alexander@yahoo.de>
2021-01-10 13:05:10 +0100

    remove event listeners when input field is filled in execute code when DOM content is loaded

--- extension/fuzzySearch.js
+++ extension/fuzzySearch.js
@@ -1,12 +1,43 @@
-document.body.addEventListener("keydown", appendFuzzyToTarget);
-console.log("FuzzySearch active");
+'use strict';
 
-function appendFuzzyToTarget(e) {
-    if(e.target.classList.contains("multiselect__input"))
-    {
-        if(!e.target.value.startsWith("~"))
-        {
-            e.target.value = "~" + e.target.value;
-        }
+const afterDOMLoaded = () => {
+  // var to store all input fields
+  let inputFieldNodeList = 'undefined';
+
+  const getInputFields = () => document.querySelectorAll('.multiselect__input');
+  const addFuzzyToNode = (e) => {
+    if (!e.target.value.startsWith('~')) {
+      e.target.value = '~' + e.target.value;
+      // remove the event listener when the user wrote something into the field to prevent input lag
+      e.target.removeEventListener('keydown', addFuzzyToNode);
     }
+  };
+
+  const wait = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  // fetch all input fields and only attach listeners when input field is empty
+  // already attached listeners will be overwritten
+  const restoreListeners = async () => {
+    inputFieldNodeList = getInputFields();
+    inputFieldNodeList.forEach((selectInput) => {
+      if (selectInput.value == '')
+        selectInput.addEventListener('keydown', addFuzzyToNode, false);
+    });
+
+    // wait a second before overwriting new listeners
+    await wait(1000);
+
+    restoreListeners();
+  };
+
+  // start here
+  restoreListeners();
+};
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', afterDOMLoaded, false);
+} else {
+  afterDOMLoaded();
 }
